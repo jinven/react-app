61c51082856b0d3a0e6196a891ca5ab50cc5fcca
a <a@A>
2020-01-05 23:26:59 +0800

    'lang'

--- src/components/Language.js
+++ src/components/Language.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import '../assets/css/language.scss'
 import { Radio } from 'antd';
-import { FormattedMessage  } from 'react-intl';
-export default class Language extends React.Component{
+import { FormattedMessage, injectIntl  } from 'react-intl';
+
+const styles = `
+article {
+    text-align: left;
+    color: #1d1d1d;
+    font-size: 20px;
+    margin: 20px 30px 30px;
+    text-shadow: rgba(255, 255, 255, 0.15) 1px 2px;
+    border: 1px solid #fff;
+    border-radius: 3px;
+    padding: 5px 10px;
+    box-shadow: rgba(0, 0, 0, 0.26) 0 0 10px 0;
+}
+`
+export default injectIntl(class Language extends React.Component{
     onChange = e => {
         this.props.onChangeLangs(e.target.value===1?'zh':'en');
     };
     render(){
         const value = this.props.langs==='zh'?1:2;
+        const {intl} = this.props;
+        let fiction = intl.formatMessage({id: 'fiction'});
+        let arrPs = [];
+        if(fiction){
+            fiction.split('\n').forEach(function(a){
+                arrPs.push(<p>{a}</p>)
+            })
+        }
         return (
             <div>
                 <h2><FormattedMessage id="language" /></h2>
@@ -17,7 +39,13 @@ export default class Language extends React.Component{
                         <Radio value={2}>English</Radio>
                     </Radio.Group>
                 </div>
+                <div>
+                    <article>
+                        {arrPs}
+                    </article>
+                </div>
+                <style jsx>{styles}</style>
             </div>
         )
     }
-}
+})
--- src/locale/en_US.js
+++ src/locale/en_US.js
@@ -15,6 +15,131 @@ const en_US = {
     logout: 'Logout',
     click_time: 'Clicked: {num} times',
     about_page: 'This is an about page',
+    fiction: `
+Tonight, humans will try to break the quark.
+
+This feat will be done at the Oriental Nuclear Centre in Lop Nur. The nuclear center looks like only a group of elegant white buildings in the desert. A huge accelerator is built in the tunnel deep in the desert. The accelerator has a circumference of 150 kilometers. A 1 million-kilowatt nuclear power plant was built nearby to supply power to the accelerator, but it is not enough to complete today's tests, and electricity can only be temporarily transferred from the Northwest Power Grid. Today, the accelerator will accelerate particles to 20 FGV, which is the energy at the beginning of the Big Bang, the energy at the creation of all things, and in this unimaginable energy, the smallest unit of matter currently known The quark will be smashed, and humans will see the deepest secrets of the material world.
+
+There are not many people in the control hall of the triton nuclear center. Among them are the two most outstanding theoretical physicists in the world. They represent two different schools of research on the deep structure of matter. One of them is American Herman. Jones, he believes that quark is the smallest unit of matter and cannot be broken; the other is Chinese Ding Yi, and his theory is that matter is infinitely separable. In the control hall are the chief engineer responsible for the operation of the accelerator, as well as a few reporters. Many other staff members are in dozens of sub-control rooms deep underground, and the control hall can only see the integrated data. The most amazing person here is a Kazakh shepherd named Di Shati. His village is within the circumference of the nuclear accelerator. At yesterday's picnic, the physicists ate his roasted whole sheep. And insisted to invite him. They think that this great moment in physics is also a great moment for all human beings, so there should be a person who knows the least about physics.
+
+The plutonium accelerator has been activated, and the energy curve on the large display screen is crawling lazily like a newly awakened earthworm, rising towards the red line marking the critical energy, which is the energy required to crush the quark.
+
+"Why isn't the TV broadcast?" Ding Yi pointed at a TV in the corner of the hall and asked, a TV football game was being broadcast. The physicist has been wearing a blue overall since he was here in Beijing and can easily be mistaken for a handyman.
+
+"Dr. Ding, we are not the center of the world. After the test results come out, it is good to have a 30-second piece of news." The chief engineer said.
+
+"Numb, incredibly numb." Ding Yi shook her head and said.
+
+"But it is necessary for survival." Jones said that he was dressed in a decadent style, with long hair, and occasionally took a silver bottle from his pocket to take a sip. "Unfortunately I'm not numb, so it's hard to survive." He said, pulling out a piece of paper, shaking in the air, "Gentlemen, this is my testament."
+
+The slang was shocked, and the reporters immediately surrounded Jones.
+
+"After this experiment is over, there will no longer be any secrets to explore in the physical world. Physics will end in an hour! I am here to meet the future of my world, my physics, you cruel lover, you I can live as long as I'm exhausted! "
+
+Ding Yi said disapprovingly: "This has been said in the Newton and Einstein era, such as Max. Born and Stephen Hawking in the last century, but physics is not over, and it will not be over in the future. You are very You will soon see that the quark will be broken, and we will step on another step on the ladder to nothing. I am here to welcome the morning of our world! "
+
+"You are copying Mao Zedong's theory, Dr. Ding, who proposed the idea of ​​infinitely separable materials in the 1950s." Jones retorted.
+
+"You are too obsessed with your thoughts." The chief engineer interjected and said, "Through the different projections of sunlight at the same time in the dry wells of Egypt and Greece, we can infer that the earth is round, and even calculate it from this Diameter, but only Magellan's trip is truly exciting. You theoretical physicists were just staying in the well before, and today we are going to do a true world cruise in the microcosm! "
+
+On the big screen, the energy curve approaches that red line. The outside world seemed to be aware of the huge energy surging in the depths of the desert. A flock of birds flew from the red willow bushes, hovering in the night sky for a long time, and wolves came in the distance. ． ． ． ． ． Finally, the energy curve crosses the red line, and the particles in the accelerator have obtained the energy required to hit the quark, which is the highest energy particle ever obtained by humans. The control computer immediately led these superenergy particles out of the 150-circle road of the accelerator, entered a branch line, and flew towards the target at a speed close to the speed of light. Under the bombardment of this extreme energy, the target immediately burst into a torrential rain of particle radiation. Countless sensors stared at the storm with their eyes wide open.They can instantly discern several raindrops of slightly different colors in the storm. It is from the combination of these raindrops that the supercomputer will determine whether an impact has occurred. The quark incident, and further determine whether the quark was broken.
+
+Super granules are continuously produced, the impact in the accelerator continues, and people are waiting nervously.
+
+The chance of super particles hitting the quark is very small, they don't know how long to wait.
+
+"Oh, friends from afar," said Elder Dischati, breaking the silence, "I was here more than ten years ago when I started the construction. At that time there were tens of thousands of people on the construction site, steel and cement were piled like mountains. High, there are hundreds of coils as tall as the building, they told me that it is an electromagnet ... I don't understand, so much money and things, so much manpower, how much desert can be irrigated, so there Overgrown with grapes and cantaloupe, but no one understands what you do. "
+
+"Master Di Shati, we are seeking the deepest secret of the material world, which is more important than anything!" Ding Yi said.
+
+"I haven't read many books, but I know that you, the most learned people in the world, are looking for the smallest grain of sand in the world."
+
+The old Kazakh shepherd's excellent definition of particle physics excited everyone.
+
+"Wonderful!" Jones yelled after getting the translation, "he thinks," he pointed to Ding Yi, "as small as a grain of sand is, as small as it is; and I think that there is the smallest grain of sand, this grain of sand cannot No matter how small, it is impossible to smash it with the most powerful hammer. Dear Master Dixati, who do you think we are right? "
+
+After listening to the translation, Di Shati shook his head, "I don't know, you can't know, what is going on in the world, how can mortals figure it out?"
+
+"So, are you an agnostic?" Ding Yi asked.
+
+The weathered eyes of the old shepherd were immersed in dreams and memories, "The world is really unthinkable! From an early age, I drove the sheep to find grass in the boundless Gobi Desert. How many nights I and the sheep lie in the wild Look at the stars. The stars are dense, shiny, like the gems in the girl's black hair; when it is not dark, the Gobi underneath is still hot, the wind is breezy, like it The breath ... At this time the world is alive, just like a big sleeping baby. At this time, without ears, but listen attentively, you can hear a voice, which is filled between heaven and earth, that is Allah Voice, only he knows what is going on in the world. "
+
+At this time, the buzzer sounded loudly. This is a signal that a quark collision event has occurred. People are turning to the big screen. The final judgment day of physics is here. The question that humans have argued for 3,000 years will have an answer soon. Supercomputer analysis data flooded the screen like a flood. The two theoretical physicists immediately discovered that something was wrong, and they shook their heads in confusion.
+
+The results did not show that the quark was broken, but it did not show that it remained intact.
+
+Suddenly, someone screamed. It was Shaditi. Only he was not interested in the data of the quark hit on the big screen. He was still standing by the window. "Oh my God, what's going on outside, come and see!"
+
+"Master Shaditi, please don't disturb us!" Said the chief engineer impatiently, but another word from Shaditi made everyone turn around.
+
+"Heaven ... what's wrong with God !!"
+
+A white light came through the window, and the people in the hall looked out. They didn't believe their eyes: the whole night sky turned milky! People rushed out of the hall, outside, on the vast Gobi, with a milky sky Soft white light, like a sea of ​​milk, the earth seems to be at the center of a huge white eggshell! When people's eyes adapt to these, they find clusters of small black spots in the milky sky, and carefully observe those black spots After their position, they are really going crazy.
+
+"Allah, those black dots ... are the stars !!" Shadyti shouted the conclusion everyone saw but couldn't believe it.
+
+They are watching negatives of the universe.
+
+In shock, someone noticed the TV in the lobby that was broadcasting the game from the window, and the situation on the screen proved that they were not dreaming: the stadium thousands of miles away was also shrouded in white light and tens of thousands of people in the stands All looked at the sky in horror. ． ． ． ． ．
+
+"When did this happen?" Asked the calm chief engineer first.
+
+"When the rumbling sounded just now," said Shaditi.
+
+People were silent. They focused their eyes on Jones and Ding Yi, hoping that the two most outstanding physicists since Einstein could make even a little explanation of the nightmare reality in front of them. The two physicists don't look at the sky anymore, they are thinking with their heads down. Ding Yi first looked up at the milky universe and exhaled for a long time.
+
+"We should have thought of it."
+
+Jones also looked up and looked at Ding Yi: "Yes, this is the meaning of that variable in the super unified theoretical equation!"
+
+"What are you talking about ?!" the chief engineer called.
+
+"Engineer, our global voyage was successful!" Ding Yi said with a smile. "You mean, our experiments led to all this ?!"
+
+"The fact is!" Jones said, and pulled out the silver wine bottle, "Now Magellan knows that the earth is round."
+
+"Round ...!?" The others looked at the two physicists in confusion.
+
+"The earth is round. If you walk straight forward from any point on its surface, you will return to the original point. Now we know the shape of the space and time of the universe. It is very similar. We go all the way to the microscopic depths. "It returned to the whole macro. The accelerator just penetrated the smallest structure of matter, so its force acted on the largest structure and reversed the entire universe." Jones explained.
+
+Ding Yi said, "Dr. Jones, you can live on. Physics is not over. It has only just begun. It is like geography has just begun after humans know the shape of the earth. We are all wrong. To say the closest to the facts, It was made by Uncle Shadice just now. Although I do not believe in Allah, the profound magic of the universe is far beyond our imagination. "
+
+"I remember, in the last century, the Englishman Arthur Clark put forward the concept of negative universe in science fiction, but who would have thought that it would become a reality?"
+
+"But what now?" Asked the chief engineer.
+
+"It's fine now, I'd love to live in the negative universe, it's as beautiful as it was before the inversion, isn't it?"
+
+Jones drank the wine in the bottle, and drunkenly opened his arms to embrace the whole new universe.
+
+"But you can see ..." The chief engineer pointed out the TV in the lobby from the window, and the terrible turmoil in the stadium was intensifying, and a collective hysteria spread in the crowd. From this picture, you can imagine that the entire human world is in a state of chaos.
+
+"Continue to bombard the target." Ding Yi told the chief engineer. After the first quark collision event, in order to analyze the results, the control computer has stopped the bombardment of the target by the super-energy particles.
+
+"Are you crazy ?! The ghost knows what effect the second quark collision event will have? Maybe it will cause the universe to collapse or explode!"
+
+"No! The previous phenomenon has proved the correctness of the super-uniform equation, and we know what will happen next time." Jones said.
+
+The super-energy particles in the plutonium accelerator are directed to the target again, and people are expecting the appearance of a few drops of raindrops of different colors in the storm of particles.
+
+1 minute, 2 minutes. ． ． ． ． ． 10 minutes. ． ． ． ． ．
+
+Various curves and data are lazily scrolling on the big screen, nothing happens.
+
+On the TV screen, the sea of ​​people in the stadium has lost control. Under the milky sky, people rammed without aim and trampled on each other. ． ． ． ． ． The image flickered, the TV signal was interrupted, and there was only a desert-like snowflake on the screen. The abrupt change of the universe is beyond all human knowledge and imagination, beyond their mental capacity, and the world is on the brink of madness. The buzzer sounded a second time, and the quark was hit a second time.
+
+There is no warning, faster than the blink of an eye, the universe is reversed again, the dark night sky, the crystal stars, and the human universe is back.
+
+"Oh my God, you're doing Allah's business!" Said Uncle Dischati. The people in the center of the nucleus now gathered on the Gobi Desert outside, under the intoxicating starry sky.
+
+"Yeah, the unremitting exploration of the material origin gives us the power of God, which is something we never dreamed of." Jones said.
+
+"But we are still human. Who knows what will happen in the future?" Ding Yi said.
+
+In the night sky, the stars are brilliant, and the inaudible music fills the entire universe.
+
+"Allah ..." Uncle Dischati lay down against the stars.`
+
     //... ...
 }
 export default en_US;
